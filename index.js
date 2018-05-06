161c0e54ed62611ac1f72d85b9230fe68557082f
Wendy Yuchen Sun <wendyyuchensun@gmail.com>
2018-05-06 18:31:45 +0800

    Add more sample events

--- index.js
+++ index.js
@@ -9,6 +9,38 @@ const events = [
     title: 'cook dinner',
     time: '2018.5.6 19:30',
   },
+  {
+    title: 'buy bread',
+    time: '2018.5.8 19:00',
+  },
+  {
+    title: 'watch movie',
+    time: '2018.5.12 22:00',
+  },
+  {
+    title: 'call mom',
+    time: '2018.5.11 20:00',
+  },
+  {
+    title: 'mop floor',
+    time: '2018.5.13 15:00',
+  },
+  {
+    title: 'do laundry',
+    time: '2018.5.13 15:30',
+  },
+  {
+    title: 'read novel',
+    time: '2018.5.9 00:00',
+  },
+  {
+    title: 'wash dishes',
+    time: '2018.5.10 21:00',
+  },
+  {
+    title: 'pay eletricity bill',
+    time: '2018.5.10 21:30',
+  },
 ]
 
 const createTitleNode = (title) => {
