ea233a1067b01ba8aa71410fd00015da1576cfbf
Wendy Yuchen Sun <wendyyuchensun@gmail.com>
2018-05-13 14:14:34 +0800

    Add other gates

--- src/gates.js
+++ src/gates.js
@@ -1,6 +1,39 @@
+const sanityCheck = input => {
+  if (input !== 0 && input !== 1) throw new Error('Input should be 0 or 1.');
+}
+
 const nand = (a, b) => {
+  sanityCheck(a)
+  sanityCheck(b)
+
   if (!a || !b) return 1;
   return 0;
 }
 
-if (typeof module !== 'undefined') module.exports = nand
+const not = input => nand(input, 1)
+
+const and = (a, b) => not(nand(a, b))
+
+const or = (a, b) => nand(nand(a, a), nand(b, b))
+
+const xor = (a, b) => {
+  const c = nand(a, b)
+  return nand(nand(a, c), nand(b, c))
+}
+
+const mux = (a, b, sel) => and(a, not(sel)) || and(b, sel)
+
+const dmux = (input, sel) => ({
+  a: and(input, not(sel)),
+  b: and(input, sel),
+})
+
+if (typeof module !== 'undefined') module.exports = {
+  nand,
+  not,
+  and,
+  or,
+  xor,
+  mux,
+  dmux,
+}
