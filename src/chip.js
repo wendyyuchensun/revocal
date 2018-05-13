9e44fb7c37a5daccd08656c4a280f56741e30057
Wendy Yuchen Sun <wendyyuchensun@gmail.com>
2018-05-13 16:27:02 +0800

    Use 0/1 to represent status of bit

--- src/chip.js
+++ src/chip.js
@@ -1,6 +1,9 @@
 const chip = on => {
   const div = document.createElement('div')
   div.classList.add('chip')
-  if (on) div.classList.add('on')
+
+  let content = on ? '1' : '0'
+  div.appendChild(document.createTextNode(content))
+
   document.querySelector('.app').appendChild(div)
 }
