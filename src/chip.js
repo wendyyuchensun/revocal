4e276349926d33ef49c8e86871e4efbf8077d656
Wendy Yuchen Sun <wendyyuchensun@gmail.com>
2018-05-17 00:48:51 +0800

    Fix view

--- src/chip.js
+++ src/chip.js
@@ -1,10 +1,1 @@
-const chip = on => {
-  const div = document.createElement('div')
-  div.classList.add('chip')
-  if (on) div.classList.add('on')
-
-  let content = on ? '1' : '0'
-  div.appendChild(document.createTextNode(content))
-
-  return div
-}
+const chip = bit => document.createTextNode(bit)
