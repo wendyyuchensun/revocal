eac088e16728dab3c16c27213a0384921e1500f5
Wendy Yuchen Sun <wendyyuchensun@gmail.com>
2018-05-20 19:20:28 +0800

    Add single-bit input cases in gate spec

--- spec/gatesSpec.js
+++ spec/gatesSpec.js
@@ -76,24 +76,48 @@ assert.equal(binary2Decimal([1, 1]), 3)
 assert.equal(binary2Decimal([1, 0, 0]), 4)
 
 // nand
+assert.deepEqual(nand([0], [0]), [1])
+assert.deepEqual(nand([0], [1]), [1])
+assert.deepEqual(nand([1], [0]), [1])
+assert.deepEqual(nand([1], [1]), [0])
 assert.deepEqual(nand(sampleBus1, sampleBus2), [1, 1])
 
 // not
+assert.deepEqual(not([0]), [1])
+assert.deepEqual(not([1]), [0])
 assert.deepEqual(not(sampleBus1), [1, 1])
 assert.deepEqual(not(sampleBus2), [0, 0])
 
 // and
 assert.deepEqual(and([0], [0]), [0])
+assert.deepEqual(and([0], [1]), [0])
+assert.deepEqual(and([1], [0]), [0])
+assert.deepEqual(and([1], [1]), [1])
 assert.deepEqual(and(sampleBus1, sampleBus2), [0, 0])
 
 // or
+assert.deepEqual(or([0], [0]), [0])
+assert.deepEqual(or([0], [1]), [1])
+assert.deepEqual(or([1], [0]), [1])
 assert.deepEqual(or([1], [1]), [1])
 assert.deepEqual(or(sampleBus1, sampleBus2), [1, 1])
 
 // xor
+assert.deepEqual(xor([0], [0]), [0])
+assert.deepEqual(xor([0], [1]), [1])
+assert.deepEqual(xor([1], [0]), [1])
+assert.deepEqual(xor([1], [1]), [0])
 assert.deepEqual(xor(sampleBus1, sampleBus2), [1, 1])
 
 // mux
+assert.deepEqual(mux([0], [0], [0]), [0])
+assert.deepEqual(mux([0], [0], [1]), [0])
+assert.deepEqual(mux([0], [1], [0]), [1])
+assert.deepEqual(mux([0], [1], [1]), [1])
+assert.deepEqual(mux([1], [0], [0]), [0])
+assert.deepEqual(mux([1], [0], [1]), [1])
+assert.deepEqual(mux([1], [1], [0]), [0])
+assert.deepEqual(mux([1], [1], [1]), [1])
 assert.deepEqual(mux([0], sampleBus1, sampleBus2), [0, 0])
 assert.deepEqual(mux([1], sampleBus1, sampleBus2), [1, 1])
 assert.deepEqual(mux([0, 0], sampleBus1, sampleBus2, sampleBus3, sampleBus4), [0, 0])
@@ -102,6 +126,11 @@ assert.deepEqual(mux([1, 0], sampleBus1, sampleBus2, sampleBus3, sampleBus4), [0
 assert.deepEqual(mux([1, 1], sampleBus1, sampleBus2, sampleBus3, sampleBus4), [1, 0])
 
 // dmux
+assert.deepEqual(dmux([0], [0]), [[0], [0]])
+assert.deepEqual(dmux([0], [1]), [[1], [0]])
+assert.deepEqual(dmux([1], [0]), [[0], [0]])
+assert.deepEqual(dmux([1], [1]), [[0], [1]])
+assert.deepEqual(dmux([1], sampleBus3), [[0, 0], [0, 1]])
 assert.deepEqual(dmux([0], sampleBus3), [[0, 1], [0, 0]])
 assert.deepEqual(dmux([1], sampleBus3), [[0, 0], [0, 1]])
 assert.deepEqual(dmux([0, 0], sampleBus3), [[0, 1], [0, 0], [0, 0], [0, 0]])
